7e5cd6e6f0632e2d741be1a85a6bacbc532318df
Marcelinu <marcelinusunday@gmail.com>
2026-06-16 11:31:04 -0300

    feat(airflow): implement extraction and landing save functions

--- airflow/dags/postgres_ingestion.py
+++ airflow/dags/postgres_ingestion.py
@@ -1,5 +1,7 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow import DAG
 from datetime import datetime
+import pandas as pd
 
 dag = DAG(
     dag_id="postgres_ingestion",
@@ -7,13 +9,39 @@ dag = DAG(
     schedule=None
 )
 
-def extract_user():
-    pass
+def extract_user() -> pd.DataFrame:
+    try:
+        hook = PostgresHook(
+            postgres_conn_id="postgres_default"
+        )
 
-def save_user():
-    pass
+        usuario = hook.get_pandas_df(
+            "SELECT * FROM usuario"
+        )
+        return usuario
 
-def extract_product():
-    pass
-def save_product():
-    pass
+    except Exception as e:
+        raise e
+
+def save_user(usuarios_df: pd.DataFrame):
+    usuarios_df.to_csv("landing/usuarios.csv",
+    index=False
+    )
+
+def extract_product() -> pd.DataFrame:
+    try:
+        hook = PostgresHook(
+            postgres_conn_id="postgres_default"
+        )
+
+        produtos = hook.get_pandas_df(
+            "SELECT * FROM produto"
+        )
+        return produtos
+
+    except Exception as e:
+        raise e
+
+def save_product(produtos_df: pd.DataFrame):
+    produtos_df.to_csv("landing/produtos.csv",
+    index=False)
