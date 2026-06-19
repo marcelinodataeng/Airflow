a025789fc910a25139fb851d7c75c3d8c95bc4bf
Marcelinu <marcelinusunday@gmail.com>
2026-06-19 12:34:40 -0300

    feat: add extract and save tasks for postgres ingestion

--- airflow/dags/postgres_ingestion.py
+++ airflow/dags/postgres_ingestion.py
@@ -1,4 +1,5 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
 from airflow import DAG
 from datetime import datetime
 import pandas as pd
@@ -9,24 +10,31 @@ dag = DAG(
     schedule=None
 )
 
+
 def extract_user() -> pd.DataFrame:
     try:
         hook = PostgresHook(
             postgres_conn_id="postgres_default"
         )
 
-        usuario = hook.get_pandas_df(
+        usuarios_df = hook.get_pandas_df(
             "SELECT * FROM usuario"
         )
-        return usuario
+
+        usuarios_df.to_csv(
+            "landing/usuarios.csv",
+            index=False
+        )
+
+        return usuarios_df
 
     except Exception as e:
         raise e
 
-def save_user(usuarios_df: pd.DataFrame):
-    usuarios_df.to_csv("landing/usuarios.csv",
-    index=False
-    )
+
+def save_user():
+    pass
+
 
 def extract_product() -> pd.DataFrame:
     try:
@@ -34,14 +42,50 @@ def extract_product() -> pd.DataFrame:
             postgres_conn_id="postgres_default"
         )
 
-        produtos = hook.get_pandas_df(
+        produtos_df = hook.get_pandas_df(
             "SELECT * FROM produto"
         )
-        return produtos
+
+        produtos_df.to_csv(
+            "landing/produtos.csv",
+            index=False
+        )
+
+        return produtos_df
 
     except Exception as e:
         raise e
 
-def save_product(produtos_df: pd.DataFrame):
-    produtos_df.to_csv("landing/produtos.csv",
-    index=False)
+
+def save_product():
+    pass
+
+
+extract_user_task = PythonOperator(
+    task_id="extract_user",
+    python_callable=extract_user,
+    dag=dag
+)
+
+save_user_task = PythonOperator(
+    task_id="save_user",
+    python_callable=save_user,
+    dag=dag
+)
+
+extract_product_task = PythonOperator(
+    task_id="extract_product",
+    python_callable=extract_product,
+    dag=dag
+)
+
+save_product_task = PythonOperator(
+    task_id="save_product",
+    python_callable=save_product,
+    dag=dag
+)
+
+
+extract_user_task >> save_user_task
+
+extract_product_task >> save_product_task
