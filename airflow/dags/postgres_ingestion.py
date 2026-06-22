c0eed8447c0ba1faeb86116fac52e0e57676cd3c
Marcelinu <marcelinusunday@gmail.com>
2026-06-22 20:07:58 -0300

    (CODE REVIEW): code reviewed made in 22/06/26

--- airflow/dags/postgres_ingestion.py
+++ airflow/dags/postgres_ingestion.py
@@ -2,114 +2,52 @@ from datetime import datetime
 from pathlib import Path
 import logging
 
-import pandas as pd
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.hooks.base import BaseHook
+
+from gustavo_sdk.app.airflow_functions import postgres_to_bucket
 
 logger = logging.getLogger(__name__)
 
-POSTGRES_CONN_ID = "postgres_default"
-
-LANDING_DIR = Path("/opt/airflow/landing")
-LANDING_DIR.mkdir(
-    parents=True,
-    exist_ok=True,
-)
-
-
-def extract_table(
-    table_name: str,
-    output_file: str,
-) -> pd.DataFrame:
-    try:
-        logger.info(
-            "Starting extraction from table %s",
-            table_name,
-        )
-
-        hook = PostgresHook(
-            postgres_conn_id=POSTGRES_CONN_ID,
-        )
-
-        dataframe = hook.get_pandas_df(
-            f"SELECT * FROM {table_name}"
-        )
-
-        output_path = LANDING_DIR / output_file
-
-        dataframe.to_csv(
-            output_path,
-            index=False,
-        )
-
-        logger.info(
-            "File saved successfully: %s",
-            output_path,
-        )
-
-        return dataframe
-
-    except Exception as error:
-        logger.exception(
-            "Failed extracting table %s",
-            table_name,
-        )
-
-        raise RuntimeError(
-            f"Error extracting table {table_name}"
-        ) from error
-
-
-def extract_user() -> pd.DataFrame:
-    return extract_table(
-        table_name="usuario",
-        output_file="usuarios.csv",
-    )
-
-
-def extract_product() -> pd.DataFrame:
-    return extract_table(
-        table_name="produto",
-        output_file="produtos.csv",
-    )
-
-
-def save_user() -> None:
-    pass
-
-
-def save_product() -> None:
-    pass
-
+conn = BaseHook.get_connection("postgres_default")
+host = conn.host
+login = conn.login
+password = conn.password
+port = conn.port or 3306
+dbname = conn.schema
 
 with DAG(
-    dag_id="postgres_ingestion",
+    dag_id="ingestion_postgres_ecomercegustavo_landing",
     start_date=datetime(2026, 6, 14),
     schedule=None,
     catchup=False,
 ) as dag:
 
-    extract_user_task = PythonOperator(
-        task_id="extract_user",
-        python_callable=extract_user,
+    user_ingestion = PythonOperator(
+        task_id="user_ingestion",
+        python_callable=postgres_to_bucket,
+        op_kwargs={
+            "host": host,
+            "login": login,
+            "password": password,
+            "port": port,
+            "dbname": dbname,
+            "table_name": "usuario",
+            "bucket_prefix": "ecomercegustavo/usuario/data.csv",
+        }
     )
 
-    save_user_task = PythonOperator(
-        task_id="save_user",
-        python_callable=save_user,
+    product_ingestion = PythonOperator(
+        task_id="product_ingestion",
+        python_callable=postgres_to_bucket,
+        op_kwargs={
+            "host": host,
+            "login": login,
+            "password": password,
+            "port": port,
+            "dbname": dbname,
+            "table_name": "produto",
+            "bucket_prefix": "ecomercegustavo/produto/data.csv",
+        }
     )
-
-    extract_product_task = PythonOperator(
-        task_id="extract_product",
-        python_callable=extract_product,
-    )
-
-    save_product_task = PythonOperator(
-        task_id="save_product",
-        python_callable=save_product,
-    )
-
-    extract_user_task >> save_user_task
-    extract_product_task >> save_product_task
--- airflow/plugins/gustavo_sdk/app/airflow_functions.py
+++ airflow/plugins/gustavo_sdk/app/airflow_functions.py
@@ -1,5 +1,6 @@
 import time
 
+from airflow.dags.postgres_ingestion import host
 from gustavo_sdk.app.jobs.mysql_to_bucket import MySQLToBucketJob
 from gustavo_sdk.app.jobs.postgre_to_bucket import PostgresToBucketJob
 from gustavo_sdk.infrastructure.common.config.settings import Settings
@@ -39,31 +40,30 @@ def mysql_to_bucket() -> None:
         logger.info(f"Status: {status} | Duration: {duration:.2f}s")
 
 
-def postgres_to_bucket() -> None:
-    start_time = time.time()
-    status = "SUCCESS"
-    db = None
-
+def postgres_to_bucket(table_name,
+                       bucket_prefix,
+                       host,
+                       database,
+                       user,
+                       password,
+                       port) -> None:
     try:
-        settings = Settings()
-        db = PostgresClient(config=settings.get_postgres_config())
+
+        db = PostgresClient(host=host,
+                            database=database,
+                            user=user,
+                            password=password,
+                            port=port)
+
         db.connect()
 
         job = PostgresToBucketJob(db=db)
 
         job.run(
-            query="SELECT * FROM clientes",
-            file_name="clientes_postgres.csv",
+            query=f"SELECT * FROM {table_name}",
+            bucket_prefix=bucket_prefix
         )
 
     except Exception as err:
-        status = "FAILED"
         logger.exception(f"PostgreSQL ETL job failed: {err}")
-
-    finally:
-        if db is not None:
-            db.close()
-
-        duration = time.time() - start_time
-        logger.info(f"Status: {status} | Duration: {duration:.2f}s")
-
+        raise
